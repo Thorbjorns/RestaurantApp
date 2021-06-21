d73087f85213eb84bf3d43c6bb5ffa4624a7c76f
praca praca <valdyrhjord@gmail.com>
2021-06-21 10:59:15 +0200

    CartController and CartService changes

--- src/main/java/com/example/restaurant/service/CartService.java
+++ src/main/java/com/example/restaurant/service/CartService.java
@@ -1,8 +1,13 @@
 package com.example.restaurant.service;
 
+import com.example.restaurant.entity.Cart;
+import com.example.restaurant.entity.Pizza;
 import com.example.restaurant.repository.CartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CartService {
@@ -12,6 +17,19 @@ public class CartService {
     public CartService(CartRepository cartRepository) {
         this.cartRepository = cartRepository;
     }
+
+    public List<Cart> getAllCarts(){
+        return cartRepository.findAll();
+    }
+
+    public void addCart(Cart cart){
+        cartRepository.save(cart);
+    }
+
+    public Optional<Cart> getCartById(Long id){
+        return cartRepository.findById(id);
+    }
+
 
 
 }
